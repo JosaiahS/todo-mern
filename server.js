4cbbf2e3f904c94e6e39b74602d3cd7bfb13942f
JosaiahS <josaiahsal@gmail.com>
2024-06-26 13:35:13 -0400

    Added errorMiddleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
+const errorMiddleware = require('./middleware/errorMiddleware');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -23,6 +24,8 @@ mongoose
 
 // Use routes
 app.use('/users', userRoutes);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
